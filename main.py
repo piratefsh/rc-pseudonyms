308a1a961b8a4f74ad6bd706d92c399b40f9a2ef
Sher Minn Chong <sherminn.chong@gmail.com>
2015-11-11 15:59:17 -0500

    Code

--- main.py
+++ main.py
@@ -1,10 +1,52 @@
 from flask import Flask, request, redirect, url_for, jsonify
+import requests
+import urllib.parse
+import os
 
 app = Flask(__name__)
+app.config.from_envvar('RC_OAUTH_CLIENT_SECRETS')
+
+app.config['RC_OAUTH_REDIRECT_AUTH_URI'] = 'http://localhost:6060/access_token'
+app.config['RC_OAUTH_REDIRECT_ACCESS_URI'] = 'http://localhost:6060/access_token/received_token'
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'hello world'
+    params = {
+        "client_id": app.config['RC_OAUTH_CLIENT_ID'],
+        "redirect_uri": app.config['RC_OAUTH_REDIRECT_AUTH_URI'],
+        "response_type": 'code'
+    }
+
+    url_params = urllib.parse.urlencode(params)
+    oauth_url = "%s?%s" % (app.config['RC_OAUTH_AUTH_URI'], url_params)
+    return redirect(oauth_url)
+
+
+@app.route('/access_token', methods=['GET', 'POST'])
+def access_token():
+    code = request.args.get('code')
+    params = {
+        'client_id': app.config['RC_OAUTH_CLIENT_ID'],
+        'client_secret': app.config['RC_OAUTH_CLIENT_SECRET'],
+        'redirect_uri': app.config['RC_OAUTH_REDIRECT_ACCESS_URI'],
+        'grant_type': 'authorization_code',
+        'code': code
+    }
+
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    }
+
+    req = requests.post(app.config['RC_OAUTH_TOKEN_URI'], data=params)
+    return req.text
+
+
+@app.route('/access_token/received_token', methods=['GET'])
+def received_token():
+    return 'got token! %s' % request.args.get('access_token')
+
+
 
 if __name__ == '__main__':
     app.debug = True
