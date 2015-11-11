e30a2111b1470a62bcd819909392ff11f6a1952c
Sher Minn Chong <sherminn.chong@gmail.com>
2015-11-11 17:34:52 -0500

    Add get_mine func

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from flask import Flask, request, redirect, url_for, jsonify
 import requests
 import urllib.parse
 import os
+import json 
 
 app = Flask(__name__)
 app.config.from_envvar('RC_OAUTH_CLIENT_SECRETS')
-
+app.config['RC_API_URI'] = 'http://www.recurse.com/api/v1'
 app.config['RC_OAUTH_AUTH_URI'] = 'https://www.recurse.com/oauth/authorize'
 app.config['RC_OAUTH_TOKEN_URI'] = 'https://www.recurse.com/oauth/token'
 app.config['RC_OAUTH_REDIRECT_AUTH_URI'] = 'http://localhost:6060/access_token'
+
+TOKENS = {}
 
 @app.route('/', methods=['GET'])
 def index():
@@ -35,14 +38,17 @@ def access_token():
     }
 
     req = requests.post(app.config['RC_OAUTH_TOKEN_URI'], data=params)
-    return req.text
+    data = json.loads(req.text)
+    print(data)
+    token = data['access_token']
+    return str(get_mine(token))
 
-
-@app.route('/access_token/received_token', methods=['GET'])
-def received_token():
-    return 'got token! %s' % request.args.get('access_token')
-
-
+def get_mine(access_token):
+    headers = {
+        'Authorization': 'Bearer %s' % access_token
+    }
+    req = requests.get("%s/people/me" % app.config['RC_API_URI'], headers=headers)
+    return json.loads(req.text)
 
 if __name__ == '__main__':
     app.debug = True
