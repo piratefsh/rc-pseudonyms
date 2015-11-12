0a69fc99559f4392f5348ed865ad4285ec806cbb
Sher Minn Chong <sherminn.chong@gmail.com>
2015-11-11 20:43:40 -0500

    Implement batch fetching

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, jsonify
+from flask import Flask, request, redirect, url_for, jsonify, session, render_template
 import requests
 import urllib.parse
 import os
@@ -11,23 +11,27 @@ app.config['RC_OAUTH_AUTH_URI'] = 'https://www.recurse.com/oauth/authorize'
 app.config['RC_OAUTH_TOKEN_URI'] = 'https://www.recurse.com/oauth/token'
 app.config['RC_OAUTH_REDIRECT_AUTH_URI'] = 'http://localhost:6060/access_token'
 
-TOKENS = {}
+sessions = {}
 
 @app.route('/', methods=['GET'])
 def index():
-    params = {
-        "client_id": app.config['RC_OAUTH_CLIENT_ID'],
-        "redirect_uri": app.config['RC_OAUTH_REDIRECT_AUTH_URI'],
-        "response_type": 'code'
-    }
+    # if user already exists
+    if 'user' in session and session['user'] in sessions:
+        return redirect(url_for('pseudonyms'))
+    else:
+        params = {
+            "client_id": app.config['RC_OAUTH_CLIENT_ID'],
+            "redirect_uri": app.config['RC_OAUTH_REDIRECT_AUTH_URI'],
+            "response_type": 'code'
+        }
 
-    url_params = urllib.parse.urlencode(params)
-    oauth_url = "%s?%s" % (app.config['RC_OAUTH_AUTH_URI'], url_params)
-    return redirect(oauth_url)
+        url_params = urllib.parse.urlencode(params)
+        oauth_url = "%s?%s" % (app.config['RC_OAUTH_AUTH_URI'], url_params)
+        return redirect(oauth_url)
 
 
 @app.route('/access_token', methods=['GET', 'POST'])
-def access_token():
+def show():
     code = request.args.get('code')
     params = {
         'client_id': app.config['RC_OAUTH_CLIENT_ID'],
@@ -39,17 +43,57 @@ def access_token():
 
     req = requests.post(app.config['RC_OAUTH_TOKEN_URI'], data=params)
     data = json.loads(req.text)
-    print(data)
-    token = data['access_token']
-    return str(get_mine(token))
+    if 'access_token' in data: 
 
-def get_mine(access_token):
+        token = data['access_token']
+        user = get_user(token)
+
+        # save user session
+        user_email = user['email']
+
+        session['user'] = user_email
+        sessions[user_email] = token 
+
+        return redirect(url_for('pseudonyms'))
+    else:
+        return req.text
+
+@app.route('/pseudonyms', methods=['GET'])
+def pseudonyms():
+    user = session['user']
+    token = sessions[user]
+    if user in sessions:
+        user_pseudonym = get_user(token)['pseudonym']
+        batches = get_batches(token)
+        batch_people = [get_batch(token, batch["id"]) for batch in batches]
+        return render_template('pseudonym.html', batches=batch_people, user_pseudonym=user_pseudonym)
+    else:
+        return redirect(url_for('index'))
+
+def make_header(access_token):
     headers = {
-        'Authorization': 'Bearer %s' % access_token
+        'Authorization': 'Bearer %s' % access_token,
+        'Accepts' : 'application/json'
     }
+    return headers 
+
+def get_batch(access_token, batch_id):
+    headers = make_header(access_token)
+    req = requests.get("%s/batches/%d/people" % (app.config['RC_API_URI'], batch_id), headers=headers)
+    return req.json()
+
+def get_batches(access_token):
+    headers = make_header(access_token)
+    req = requests.get("%s/batches" % app.config['RC_API_URI'], headers=headers)
+    return json.loads(req.text)
+    
+
+def get_user(access_token):
+    headers = make_header(access_token)
     req = requests.get("%s/people/me" % app.config['RC_API_URI'], headers=headers)
     return json.loads(req.text)
 
 if __name__ == '__main__':
     app.debug = True
+    app.secret_key = app.config['SESSION_SECRET']
     app.run(host='0.0.0.0', port=6060)
